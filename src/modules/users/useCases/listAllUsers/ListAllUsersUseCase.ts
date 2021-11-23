ad3b0851fa249f39801f02efc1dc91cb2f18d84f
WilliamBritoFC <william.brito@fcamara.com.br>
2021-11-23 01:48:57 -0300

    completed

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,16 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userExists = this.usersRepository.findById(user_id);
+    if(!userExists) {
+      throw new Error("User not exists")
+    }
+
+    if(!userExists.admin) {
+      throw new Error("User is not a admin");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
